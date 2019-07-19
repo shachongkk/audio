b47199c92adf7da614c7934e7e858f8ec65636e9
流动 <1249444369@qq.com>
2019-07-19 23:50:17 +0800

    add method named getIndex

--- src/main/java/com/chian/audio/controller/indexController.java
+++ src/main/java/com/chian/audio/controller/indexController.java
@@ -2,14 +2,22 @@ package com.chian.audio.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class indexController {
 
 	@RequestMapping("/")
 	public String index(){
 		System.out.println("come here");
 		return "fan";
+	}
+	
+	@RequestMapping("getIndex")
+	@ResponseBody
+	public String getIndex(){
+		return "this is my first jenkins project";
 	}
 	
 }
